f7bcb01af256fcaf9564816e7a57c214b6d555f7
Marion-Lotito <marion.lotito@gmail.com>
2018-05-08 23:52:37 +0200

    feat: win the game and reset the player

--- js/app.js
+++ js/app.js
@@ -49,7 +49,12 @@ const Player = function(x, y) {
 };
 
 Player.prototype.update = function(dt) {
+  // Win the game and reset the player with a small delay
+  if (this.y < -30 && this.y < 20 ) {
+    setTimeout(function(){ player.x = 205;
+    player.y = 410; }, 500);
 
+  }
 };
 
 Player.prototype.render = function() {
