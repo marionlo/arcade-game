8942c9ace5efc3e5dc80a37622a405b01b422f8b
Marion <marion.lotito@gmail.com>
2018-05-09 09:37:30 +0200

    refactor: beautify js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 // Enemies our player must avoid
 var Enemy = function(x, y, speed) {
     // Variables to determine the axis and the speed of the enemy object
-      this.x = x;
-      this.y = y;
-      this.speed = speed;
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -12,12 +12,10 @@ var Enemy = function(x, y, speed) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-  this.x += this.speed*dt;
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    // Increment the x according to the speed and delta time (the enemies will scroll from left to right)
+    this.x += this.speed * dt;
 
-    // Reset enemies with a random speed
+    // Reset enemies on the start position with a random speed
     if (this.x > 520) {
         this.x = -100;
         this.speed = 150 + Math.floor(Math.random() * 300);
@@ -38,50 +36,64 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-
+// Creation of the Player
 const Player = function(x, y) {
     // Variables to determine the axis of the player object
-      this.x = x;
-      this.y = y;
+    this.x = x;
+    this.y = y;
     // The image/sprite for our player, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/char-horn-girl.png';
 };
 
+// Update the player's position, required method for game
+// Parameter: dt, a time delta between ticks
 Player.prototype.update = function(dt) {
-  // Win the game and reset the player with a small delay
-  if (this.y < -30 && this.y < 20 ) {
-    setTimeout(function(){
-      player.x = 205;
-      player.y = 410; }, 500);
+    // Win the game and reset the player on the start position with a small delay
+    if (this.y < -30 && this.y < 20) {
+        setTimeout(function() {
+            player.x = 205;
+            player.y = 410;
+        }, 500);
 
-  }
+    }
 };
 
+// Draw the player on the screen, required method for game
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Function to make the player moves
 // Different scenarios, use of switch case to control the player
-Player.prototype.handleInput = function (key) {
-switch (key) {
-  case "up": if (this.y > 0 ) {this.y -= 90 }
-  break;
+// Impossible to move out of the canva
+Player.prototype.handleInput = function(key) {
+    switch (key) {
+        case "up":
+            if (this.y > 0) {
+                this.y -= 90
+            }
+            break;
 
-  case "down": if (this.y < 400 ) {this.y += 90;}
-  break;
+        case "down":
+            if (this.y < 400) {
+                this.y += 90;
+            }
+            break;
 
-  case "right": if (this.x < 400 ) {this.x += 100;}
-  break;
+        case "right":
+            if (this.x < 400) {
+                this.x += 100;
+            }
+            break;
 
-  case "left": if (this.x > 10 ) {this.x -= 100;}
-  break;
-}
+        case "left":
+            if (this.x > 10) {
+                this.x -= 100;
+            }
+            break;
+    }
 };
-
-
-
 
 
 // Now instantiate your objects.
@@ -90,9 +102,13 @@ const allEnemies = [];
 
 // Place the player object in a variable called player
 const player = new Player(205, 410);
+
+// Place the enemies objects in a variables called enemyX
 const enemy = new Enemy(-100, 130, 100);
 const enemy2 = new Enemy(-260, 230, 300);
 const enemy3 = new Enemy(-360, 50, 250);
+
+// Push the 3 enemies in the empty allEnemies array
 allEnemies.push(enemy, enemy2, enemy3);
 
 
